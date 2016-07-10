6a80bbcd695583bdf5ee870ed8cebd7c8c758704
Sergio Diniz <sergiodinizsh@gmail.com>
2016-07-10 10:51:45 -0300

    change pagination to use angular methods

--- shine/app/assets/javascripts/customers_app.js
+++ shine/app/assets/javascripts/customers_app.js
@@ -2,10 +2,14 @@ var app = angular.module('customers', []);
 
 app.controller("CustomerSearchController", ["$scope", "$http", function($scope, $http){
   $scope.customers = [];
+
+  $scope.page = 0;
+
   $scope.search = function(searchTerm){
     $scope.searchedFor = searchTerm;
 
-    $http.get("/customers.json", {"params": {"keywords": searchTerm}})
+
+    $http.get("/customers.json", {"params": {"keywords": searchTerm, "page": $scope.page}})
          .then(function(response){
            $scope.customers = response.data;
          },
@@ -13,4 +17,18 @@ app.controller("CustomerSearchController", ["$scope", "$http", function($scope, 
            alert("There was a problem: " + response.status);
          });
   }
+
+  $scope.previousPage = function(){
+    if ($scope.page > 0){
+      $scope.page = $scope.page - 1;
+      $scope.search($scope.keywords);
+    }
+  }
+
+  $scope.nextPage = function(){
+    $scope.page = $scope.page + 1;
+    $scope.search($scope.keywords);
+  }
+
+  // end
 }]);
